81b74d2a0b3353831ab704d1b7ab2b6326bbc082
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-01 10:08:00 +0700

    02_1

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "Jauhar Maulana A'la <br>";
+    echo "2141720186 <br>";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID:".$id;
+});
+
+
